379e028e69c1abda66c5a503512f12a0d48c4dd6
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-25 18:17:55 -0400

    Implemented prev and next buttons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
-
 $(document).ready(function() {
   //this will be excuted when the page is ready
-  $('#slides').superslides();
+  $('#slides').superslides({
+    animation: 'fade',
+    play: 5000,
+    pagination: false
+  });
 });

fefaf5587ffedbafee0cc61eb1d56ac7d2953ac6
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-26 00:00:43 -0400

    adding about page image

--- js/script.js
+++ js/script.js
@@ -5,4 +5,13 @@ $(document).ready(function() {
     play: 5000,
     pagination: false
   });
+
+  var typed = new Typed(".typed", {
+    strings: ["Logically minded", "Passionate", "Creative at heart", "Inspired by greatness"],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
+
 });

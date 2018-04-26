d39b06bd5477f2f1e206d04ca7aba617f615f9b1
Johntae Stroud <johntae.stroud@gmail.com>
2018-04-26 09:20:39 -0400

    Implemented stats section

--- js/script.js
+++ js/script.js
@@ -7,11 +7,35 @@ $(document).ready(function() {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Logically minded", "Passionate", "Creative at heart", "Inspired by greatness"],
+    strings: ["Logically-minded", "Passionate", "Creative at heart", "Inspired by greatness"],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
     showCursor: false
   });
+
+  var statsTopOffset = $(".statsSection").offset().top;//getting the top position
+
+  var countUpFinished = false;
+
+  $(window).scroll(function() {//when scrolling, execute this code below:
+    //if we di
+     if(!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+       $(".counter").each(function() {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+
+        element.countup(endVal);
+
+        })
+
+       countUpFinished = true;
+
+      }
+  });
+
+ 
+
+ 
 
 });
